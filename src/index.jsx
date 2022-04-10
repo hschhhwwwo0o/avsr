faa6678172aa9dd3011a5b253d8c39656f72176b
Saveliy <ssandry.github@gmail.com>
2022-04-10 14:47:19 +0300

    Refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 reportWebVitals();
