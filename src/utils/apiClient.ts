d6dfe74a5d255bc1288ef4cd6be31678d305f272
Saveliy <ssandry.github@gmail.com>
2022-04-10 19:36:43 +0300

    Refactoring

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,10 +1,15 @@
-import feathers, { Application } from "@feathersjs/feathers";
+import feathers, { Application, Service } from "@feathersjs/feathers";
 import socketio from "@feathersjs/socketio-client";
 import io from "socket.io-client";
 import auth from "@feathersjs/authentication-client";
 
+interface IFeathersClient {
+  posters: Service<any>;
+  users: Service<any>;
+}
+
 const socket = io("https://abiisr-api.herokuapp.com/");
-const apiClient: Application<any> = feathers();
+const apiClient: Application<IFeathersClient> = feathers();
 
 apiClient.configure(socketio(socket));
 apiClient.configure(auth());
