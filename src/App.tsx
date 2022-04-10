13c5e7d4baf84755e0f31504699148de10862b83
Saveliy <ssandry.github@gmail.com>
2022-04-10 19:25:16 +0300

    Refactoring

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment, FunctionComponent } from "react";
+import Router from "router";
+
+const App: FunctionComponent = ({ children }) => {
+  return (
+    <Fragment>
+      <Router />
+    </Fragment>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Router from "router";
 import reportWebVitals from "reportWebVitals";
 import "assets/index.css";
+import App from "App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
 
