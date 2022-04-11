ca21f098e2158f30ed30be910dc94509ddcbc1ec
Saveliy <ssandry.github@gmail.com>
2022-04-11 12:08:20 +0300

    Initial react query provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React, { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "reportWebVitals";
 import "assets/index.css";
 import App from "App";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
 
