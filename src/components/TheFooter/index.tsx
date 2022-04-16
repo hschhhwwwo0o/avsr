c52b2d09b41e681646b601864b2a825a8ec154bc
Saveliy <ssandry.github@gmail.com>
2022-04-16 18:54:08 +0300

    Adaptive footer

--- src/components/TheFooter/index.tsx
+++ src/components/TheFooter/index.tsx
@@ -4,20 +4,20 @@ import React, { Fragment, FunctionComponent } from "react";
 const TheFooter: FunctionComponent = () => {
   return (
     <Fragment>
-      <div className="flex items-center justify-center py-16">
-        <div className="flex flex-row justify-between">
-          <div className="w-[240px]">
-            <h4 className="uppercase text-sm font-extrabold cursor-pointer">AVSR</h4>
-            <div className="uppercase text-xs font-semibold mt-2">
+      <div className="flex items-center justify-center py-10 md:py-16">
+        <div className="flex flex-col gap-6 justify-between items-center md:items-start md:gap-0 md:flex-row">
+          <div className="md:w-[240px]">
+            <h4 className="uppercase text-sm font-extrabold cursor-pointer text-center md:text-left">AVSR</h4>
+            <div className="uppercase text-xs font-semibold mt-2 text-center md:text-left">
               A Vision of Something <br /> Random
             </div>
           </div>
-          <div className="w-[240px]">
+          <div className="md:w-[240px]">
             <a href="https://unsplash.com/" className="uppercase text-sm font-extrabold cursor-pointer">
               Unsplash
             </a>
           </div>
-          <div className="w-0">
+          <div className="md:w-0">
             <a
               href="https://vercel.com/dashboard"
               className="uppercase whitespace-nowrap text-sm font-extrabold cursor-pointer"
