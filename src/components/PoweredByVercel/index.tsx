675968bb5285d9f69b5a1086d9b5c63f2ec6c94f
Saveliy <ssandry.github@gmail.com>
2022-04-16 16:25:03 +0300

    Add component

---
+++ src/components/PoweredByVercel/index.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment, FunctionComponent } from "react";
+
+const PoweredByVercel: FunctionComponent = () => {
+  return (
+    <Fragment>
+      <div className="h-12 uppercase font-semibold text-xs flex items-center justify-center bg-[#252525]">
+        Powered by Vercel
+      </div>
+    </Fragment>
+  );
+};
+
+export default PoweredByVercel;
