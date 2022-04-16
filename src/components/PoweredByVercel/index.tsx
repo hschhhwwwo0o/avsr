5cde954729e42fce1081b87d58545d97359906e4
Saveliy <ssandry.github@gmail.com>
2022-04-16 18:14:49 +0300

    Fix component

--- src/components/PoweredByVercel/index.tsx
+++ src/components/PoweredByVercel/index.tsx
@@ -3,7 +3,7 @@ import React, { Fragment, FunctionComponent } from "react";
 const PoweredByVercel: FunctionComponent = () => {
   return (
     <Fragment>
-      <div className="h-12 uppercase font-semibold text-xs flex items-center justify-center bg-[#252525]">
+      <div className="h-12 uppercase font-semibold text-xs flex items-center justify-center bg-[#252525] text-white">
         Powered by Vercel
       </div>
     </Fragment>
