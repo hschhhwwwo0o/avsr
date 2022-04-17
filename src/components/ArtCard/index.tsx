ad5e773e6505517b7d982c5b404b98b2b5260ab0
Saveliy <ssandry.github@gmail.com>
2022-04-17 13:10:13 +0300

    Arts fixs

--- src/components/ArtCard/index.tsx
+++ src/components/ArtCard/index.tsx
@@ -4,7 +4,7 @@ const ArtCard: FunctionComponent = () => {
   return (
     <Fragment>
       <div className="p-8 border-r border-b border-[#EBEBEB] h-full cursor-pointer">
-        <div className="p-10 h-[25vw]">
+        <div className="p-4 h-[70vw] sm:p-10 sm:h-[40vw] lg:h-[25vw]">
           <img
             className="h-full w-full object-cover"
             src="https://images.unsplash.com/photo-1650178294065-b396e503853d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -3,7 +3,7 @@ import React, { Fragment, FunctionComponent } from "react";
 const Grid: FunctionComponent = ({ children }) => {
   return (
     <Fragment>
-      <div className="grid grid-cols-3 border-[#EBEBEB] border-l border-t">{children}</div>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 border-[#EBEBEB] border-l border-t">{children}</div>
     </Fragment>
   );
 };
