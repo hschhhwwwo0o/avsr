4731ac065f4b7b5389418480d70b9c88b78e084a
Saveliy <ssandry.github@gmail.com>
2022-04-17 10:36:44 +0300

    Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment, FunctionComponent, ReactNode } from "react";
+
+interface IButton {
+  children: ReactNode;
+  w?: string;
+}
+
+const Button: FunctionComponent<IButton> = ({ children, w = "" }) => {
+  return (
+    <Fragment>
+      <button className={`bg-[#252525] text-white uppercase py-4 px-11 font-semibold ${w}`}>{children}</button>
+    </Fragment>
+  );
+};
+
+export default Button;
