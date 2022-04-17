46759ba8d3351edfb9a30eb2459473e9518c7b54
Saveliy <ssandry.github@gmail.com>
2022-04-17 13:43:22 +0300

    Fix header position

--- src/components/TheHeader/index.tsx
+++ src/components/TheHeader/index.tsx
@@ -12,7 +12,8 @@ const TheHeader: FunctionComponent = () => {
   return (
     <Fragment>
       <div>
-        <header className="h-[65px] border-[#EBEBEB] border-b flex items-center justify-center px-5 2xl:px-0">
+        <div className="h-[65px]"></div>
+        <header className="h-[65px] border-[#EBEBEB] border-b flex items-center justify-center px-5 fixed top-0 w-screen z-50 2xl:px-0">
           <div className="max-w-[1500px] w-full flex justify-between">
             <ul className="flex flex-row gap-10 items-center">
               <li className="text-sm font-medium uppercase hidden md:block">
@@ -33,7 +34,7 @@ const TheHeader: FunctionComponent = () => {
             </div>
           </div>
         </header>
-        <div className="h-[65px] flex items-center justify-center -mt-[65px]">
+        <div className="h-[65px] flex items-center justify-center fixed top-0 left-0 w-screen z-40 bg-white">
           <Logo />
         </div>
       </div>
