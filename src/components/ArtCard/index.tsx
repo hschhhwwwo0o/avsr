a5ddbba7ddbee1bd31be0216eff7d714d1785393
Saveliy <ssandry.github@gmail.com>
2022-04-18 16:22:48 +0300

    Fix image on art card

--- src/components/ArtCard/index.tsx
+++ src/components/ArtCard/index.tsx
@@ -4,10 +4,10 @@ const ArtCard: FunctionComponent = () => {
   return (
     <Fragment>
       <div className="p-8 border-r border-b border-[#EBEBEB] h-full cursor-pointer">
-        <div className="p-4 h-[70vw] sm:p-10 sm:h-[40vw] lg:h-[25vw]">
+        <div className="p-4 h-[70vw] flex items-center justify-center sm:p-10 sm:h-[40vw] lg:h-[25vw]">
           <img
-            className="h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1650178294065-b396e503853d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            className="object-cover"
+            src="https://images.unsplash.com/photo-1650208312823-4a676f114aff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
             alt=""
           />
         </div>
