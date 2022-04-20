f9e8b220f40ffbdce2577735c31aa376cf7641e6
Saveliy <ssandry.github@gmail.com>
2022-04-20 15:39:10 +0300

    Fix localizations

--- src/components/MoreArtsBy/index.tsx
+++ src/components/MoreArtsBy/index.tsx
@@ -2,11 +2,14 @@ import ArtCard from "components/ArtCard";
 import Grid from "components/Grid";
 import GridLayout from "layouts/GridLayout";
 import React, { Fragment, FunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
 
 const MoreArtsBy: FunctionComponent = () => {
+  const { t } = useTranslation();
+
   return (
     <Fragment>
-      <div className="py-10 text-center uppercase font-semibold">More Arts by Itoko Toma</div>
+      <div className="py-10 text-center uppercase font-semibold">{t("More arts by")} Itoko Toma</div>
       <GridLayout>
         <Grid>
           <ArtCard />
