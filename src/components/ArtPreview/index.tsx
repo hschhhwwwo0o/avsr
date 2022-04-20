f836953b6d2c796e8f2c4e7b1ca8b04dafcf3d2d
Saveliy <ssandry.github@gmail.com>
2022-04-20 13:44:23 +0300

    Fixs borders

--- src/components/ArtPreview/index.tsx
+++ src/components/ArtPreview/index.tsx
@@ -6,8 +6,8 @@ const ArtPreview: FunctionComponent = () => {
   return (
     <Fragment>
       <div className="lg:h-[849px] w-full flex flex-col lg:flex-row">
-        <div className="lg:w-[70%] h-full bg-[#FCFCFC] flex items-center justify-center">
-          <div className="w-[80vw] h-[80vw] lg:w-[642px] lg:h-[642px] bg-white flex items-center justify-center">
+        <div className="lg:w-[70%] border-[#EAEAEA] border-b h-full bg-[#FCFCFC] flex items-center justify-center">
+          <div className="w-[80vw] h-[80vw] border-[#EAEAEA] border lg:w-[642px] lg:h-[642px] bg-white flex items-center justify-center">
             <img
               src="https://images.unsplash.com/photo-1650208312823-4a676f114aff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
               className="w-[60vw] h-[60vw] lg:w-[518px] lg:h-[518px]"
@@ -15,7 +15,7 @@ const ArtPreview: FunctionComponent = () => {
             />
           </div>
         </div>
-        <div className="lg:w-[30%] h-full flex items-center justify-center px-4 lg:px-16 py-10">
+        <div className="lg:w-[30%] border-[#EAEAEA] border-l border-b h-full flex items-center justify-center px-4 lg:px-16 py-10">
           <div>
             <h2 className="text-2xl lg:text-4xl font-semibold">A Brief Inquiry Into Online Relationships</h2>
             <NavLink to="/user/0" className="flex flex-row gap-4 items-center mt-4">
