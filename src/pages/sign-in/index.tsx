fdaec62651b0652d63ac2a106538c2c5e0155500
Saveliy <ssandry.github@gmail.com>
2022-04-20 14:23:34 +0300

    Localization

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -3,23 +3,26 @@ import { Page } from "types";
 import TheLayout from "layouts";
 import Input from "components/Input";
 import Button from "components/Button";
+import { useTranslation } from "react-i18next";
 
 const SignInPage: Page = () => {
+  const { t } = useTranslation();
+
   return (
     <Fragment>
       <TheLayout>
         <div className="flex flex-col-reverse lg:flex-row lg:h-[688px]">
           <div className="flex items-center justify-center lg:w-1/2">
             <div className="w-[432px] pt-8 lg:pt-0">
-              <span className="block uppercase text-center font-bold text-xl">Create an Account</span>
+              <span className="block uppercase text-center font-bold text-xl">{t("Sign in")}</span>
               <div className="grid gap-3 mt-8 lg:mt-14">
                 <Input />
                 <Input />
                 <span className="w-full mt-2">
-                  <Button w="w-full">Create an Account</Button>
+                  <Button w="w-full">{t("Sign in")}</Button>
                 </span>
                 <span className="uppercase text-xs font-semibold">
-                  Already have an account? <span className="underline">Sign in</span>
+                  {t("Don't have an account yet?")} <span className="underline">{t("Create an Account")}</span>
                 </span>
               </div>
             </div>
