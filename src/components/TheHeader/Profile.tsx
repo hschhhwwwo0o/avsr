5b581d6dd619d8ae90883b4c315b41034b9347ed
Saveliy <ssandry.github@gmail.com>
2022-04-22 14:48:48 +0300

    Refactoring

--- src/components/TheHeader/Profile.tsx
+++ src/components/TheHeader/Profile.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import User from "store/User";
+import Avatar from "components/Avatar";
 
 const Profile: FunctionComponent = () => {
   const { t } = useTranslation();
@@ -10,9 +11,14 @@ const Profile: FunctionComponent = () => {
   return (
     <Fragment>
       <div>
-        {!User.user.email && (
+        {!User.user.email ? (
           <span className="uppercase text-sm font-medium cursor-pointer">
             <NavLink to="/create-an-account">{t("Authorization")}</NavLink>
+          </span>
+        ) : (
+          <span className="flex flex-row gap-3 items-center">
+            <span className="uppercase text-sm font-semibold">Profile</span>
+            <Avatar />
           </span>
         )}
       </div>
